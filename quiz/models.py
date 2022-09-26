7bb6abd7c3c8ef73605f378162c86f10636fc6d9
mehmet <tas349873@gmail.com>
2022-09-26 20:22:07 +0300

    update

--- quiz/models.py
+++ quiz/models.py
@@ -17,3 +17,15 @@ class Category(models.Model):
 class Quiz(models.Model):
     title= models.CharField(max_length=50,verbose_name='Quiz title')
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = 'Quizes'
+
+
+class Question(models.Model)
