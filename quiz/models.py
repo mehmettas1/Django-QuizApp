5c29801ac7cefe4c1417c9b0066b8d636572ba1d
mehmet <tas349873@gmail.com>
2022-09-26 20:17:42 +0300

    update

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,19 @@
+from msilib.schema import Class
+from tabnanny import verbose
+from turtle import title
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+class Quiz(models.Model):
+    title= models.CharField(max_length=50,verbose_name='Quiz title')
+    cate
