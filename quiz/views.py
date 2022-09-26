b6af5d214095ee5f7300f25d1fc941740d5ded55
mehmet <tas349873@gmail.com>
2022-09-26 21:27:27 +0300

    update

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import (
+    Category,
+    Quiz,
+    Question,
+    Option
+)
+
+class CategoryList(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = ''
